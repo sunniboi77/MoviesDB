319dd4921963527a182affccda49c3b5dc45e7b7
sunniboi <attila.szaniszlo@gmail.com>
2022-06-13 16:55:42 +0200

    cors updated

--- server_movie_mongo_2.10.js
+++ server_movie_mongo_2.10.js
@@ -33,7 +33,7 @@ app.use(express.static("public")); // serve static files
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080','http://localhost:1234', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
